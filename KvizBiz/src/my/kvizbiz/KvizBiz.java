3550ee86e88a3cda83a975d46aa5aca11ba99bba
Faruk Ljuca <fljuca1@etf.unsa.ba>
2015-04-22 21:52:20 +0200

    Dodao prazan projekat

---
+++ KvizBiz/src/my/kvizbiz/KvizBiz.java
@@ -0,0 +1,65 @@
+package my.kvizbiz;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JTextField;
+
+import java.awt.GridBagConstraints;
+
+import javax.swing.JButton;
+
+import java.awt.Insets;
+
+import javax.swing.JLabel;
+
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+
+public class KvizBiz {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					KvizBiz window = new KvizBiz();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public KvizBiz() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 250, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0};
+		gridBagLayout.rowHeights = new int[]{0};
+		gridBagLayout.columnWeights = new double[]{Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+	}
+
+}
