c1d18b09f6327a51e02c8c10add47f5524bc41b5
Faruk Ljuca <fljuca1@etf.unsa.ba>
2015-05-19 17:07:49 +0200

    Malo nesto radio

--- glavna_aplikacija/src/main/java/ba/tim8/kvizbiz/forme/frmDodavanjePitanja_v2.java
+++ glavna_aplikacija/src/main/java/ba/tim8/kvizbiz/forme/frmDodavanjePitanja_v2.java
@@ -1,13 +1,11 @@
 package ba.tim8.kvizbiz.forme;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.*;
+import javax.swing.border.*;
+
 import net.miginfocom.swing.MigLayout;
-import javax.swing.JButton;
 
 public class frmDodavanjePitanja_v2 extends JFrame {
 
@@ -36,10 +34,36 @@ public class frmDodavanjePitanja_v2 extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout());
+
+		JPanel kontejner = new JPanel(new MigLayout("", "[grow]", "[grow,fill][grow,bottom][][::100px,fill]"));
+		kontejner.setBorder(null);
+		contentPane.add(kontejner, BorderLayout.CENTER);
 		
+		JPanel pnlPodaci = new JPanel();
+		pnlPodaci.setBorder(new TitledBorder("Unesite podatke o pitanju:"));
+		kontejner.add(pnlPodaci, "cell 0 0,growx,aligny top");
 		
+		JPanel pnlOdgovor = new JPanel();
+		kontejner.add(pnlOdgovor, "cell 0 1,grow");
+		
+		JButton btnOk = new JButton("OK");
+		kontejner.add(btnOk, "flowx,cell 0 2,alignx right,aligny bottom");
+		
+		JButton btnNewButton = new JButton("New button");
+		kontejner.add(btnNewButton, "cell 0 2,alignx right");
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		kontejner.add(btnNewButton_1, "cell 0 3,growx,aligny bottom");
+		
+		JButton btnStatus = new JButton("Statusna traka");
+		btnStatus.setEnabled(false);
+		btnStatus.setHorizontalAlignment(SwingConstants.LEFT);
+		btnStatus.setForeground(SystemColor.textHighlight);
+		contentPane.add(btnStatus, BorderLayout.SOUTH);
+		
+		/*
 		contentPane.setLayout(new MigLayout("", "[grow]", "[grow,fill][grow,bottom][][::100px,fill]"));
 		
 		JPanel panel = new JPanel();
@@ -56,8 +80,7 @@ public class frmDodavanjePitanja_v2 extends JFrame {
 		
 		JButton btnNewButton_1 = new JButton("New button");
 		contentPane.add(btnNewButton_1, "cell 0 3,growx,aligny bottom");
-		
-	
+		*/	
 	}
 
 }
