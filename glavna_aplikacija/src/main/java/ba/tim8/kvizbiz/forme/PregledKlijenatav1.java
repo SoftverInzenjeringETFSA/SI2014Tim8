c236637bd52a011a36aa20ed773166850efed50b
Orhan Ljubuncic <orhanljubuncic@yahoo.com>
2015-05-23 01:33:28 +0200

    Dodao pretragu klijenata

--- glavna_aplikacija/src/main/java/ba/tim8/kvizbiz/forme/PregledKlijenatav1.java
+++ glavna_aplikacija/src/main/java/ba/tim8/kvizbiz/forme/PregledKlijenatav1.java
@@ -1,11 +1,23 @@
 package ba.tim8.kvizbiz.forme;
 
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.table.DefaultTableModel;
 
+import ba.tim8.kvizbiz.dao.AdministratorDao;
+import ba.tim8.kvizbiz.dao.KlijentDao;
+import ba.tim8.kvizbiz.entiteti.Administrator;
+import ba.tim8.kvizbiz.entiteti.Klijent;
 import net.miginfocom.swing.MigLayout;
 
 public class PregledKlijenatav1 extends JFrame {
@@ -60,7 +72,42 @@ public class PregledKlijenatav1 extends JFrame {
 		mainContent.add(label, "cell 1 0,growx");
 		
 		kategorija = new JComboBox<String>();
-		kategorija.setModel(new DefaultComboBoxModel(new String[] {"--------", "Username", "Ime", "Prezime", "Adresa", "Datum rođenja", "Telefon", "Email"}));
+		kategorija.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e)
+			{
+				// biranje kategorije
+				
+				 //if(e.getStateChange() == ItemEvent.SELECTED)
+				//JOptionPane.showMessageDialog(null, e.getItem());
+				
+				
+				
+				
+				AdministratorDao adao = AdministratorDao.get();
+				
+				if(e.getStateChange() == ItemEvent.SELECTED)
+				{
+					
+					
+					String kategorija = e.getItem().toString();
+					
+					if(kategorija.compareTo("--------") == 0)
+					{
+						ucitajSveKlijente();
+						vrijednost.setText("");
+						
+						
+					}
+					
+						
+						
+					
+				}
+				
+				
+			}
+		});
+		kategorija.setModel(new DefaultComboBoxModel(new String[] {"--------", "Ime", "Prezime", "Adresa", "Datum rođenja", "Telefon", "Email"}));
 		
 		mainContent.add(kategorija, "cell 2 0,growx");
 		JLabel label_1 = new JLabel("Unesite vrijednost: ");
@@ -71,6 +118,43 @@ public class PregledKlijenatav1 extends JFrame {
 		mainContent.add(vrijednost, "cell 2 1,growx");
 		
 		btnPretrai = new JButton("Pretraži");
+		btnPretrai.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e)
+			
+			
+			{
+				// pretraga
+				
+				KlijentDao adao = KlijentDao.get();
+				
+				
+				if(kategorija.getSelectedItem().toString() == "Ime")
+					ucitajKlijente(adao.dajPoImenu(vrijednost.getText()));
+				else if(kategorija.getSelectedItem().toString() == "Prezime")
+					ucitajKlijente(adao.dajPoPrezimenu(vrijednost.getText()));
+				else if(kategorija.getSelectedItem().toString() == "Adresa")
+					ucitajKlijente(adao.dajPoAdresi(vrijednost.getText()));
+				else if(kategorija.getSelectedItem().toString() == "Datum rođenja")
+				{
+						try
+					{
+						ucitajKlijente(adao.dajPoDatumu(vrijednost.getText()));
+					}
+					catch(Exception ex)
+					{
+						JOptionPane.showMessageDialog(null,
+								"Datum mora biti u formatu dd/mm/yyyy",
+								"Pogrešan format datuma",
+								JOptionPane.ERROR_MESSAGE);
+					}
+				}
+				else if(kategorija.getSelectedItem().toString() == "Telefon")
+					ucitajKlijente(adao.dajPoTelefonu(vrijednost.getText()));
+				else if(kategorija.getSelectedItem().toString() == "Email")
+					ucitajKlijente(adao.dajPoMailu(vrijednost.getText()));
+			}
+		});
 		mainContent.add(btnPretrai, "cell 3 1");
 		
 		JScrollPane scrollPane = new JScrollPane();
@@ -107,6 +191,63 @@ public class PregledKlijenatav1 extends JFrame {
 		lblStatus.setBorder(BorderFactory.createLineBorder(Color.lightGray));
 		lblStatus.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblStatus, BorderLayout.SOUTH);
+		
+		ucitajSveKlijente();
+	}
+	
+	private void ucitajSveKlijente()
+	{
+		// ucitavanje svih klijenata iz baze i prikaz u tabeli
+		
+		KlijentDao kdao = KlijentDao.get();
+		Collection<Klijent> klijenti = kdao.readAll();
+		sortirajPoPrezimenu(klijenti);
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		
+		
+		
+		ucitajKlijente(klijenti);
+		
+		
+	}
+	
+	private void sortirajPoPrezimenu(Collection<Klijent> klijenti)
+	{
+		Comparator<Klijent> comparator = new Comparator<Klijent>() {
+		    public int compare(Klijent f1, Klijent f2) {
+		        return f1.get_prezime().compareTo(f2.get_prezime());
+		    }
+		};
+
+		Collections.sort((ArrayList<Klijent>) klijenti, comparator);
+		
+	}
+
+	private void ucitajKlijente(Collection<Klijent> klijenti)
+	{
+		Integer brojac = 0;
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		removeAllRows();
+		
+		for(Klijent k : klijenti)
+		{
+			model.addRow(new Object[]{k.get_ime(), k.get_prezime(), k.get_spol(), k.get_adresa(), k.get_datumRodjenja(), k.get_eMail(), k.get_telefon()});
+			brojac++;
+		}
+		
+		lblStatus.setText("Prikazano " + brojac.toString() + " rezultata.");
+		
+		lblStatus.setForeground(Color.GREEN);
+	}
+	
+	private void removeAllRows()
+	{
+		DefaultTableModel dm = (DefaultTableModel) table.getModel();
+		int rowCount = dm.getRowCount();
+	
+		for (int i = rowCount - 1; i >= 0; i--) {
+		    dm.removeRow(i);
+		}
 	}
 
 }
