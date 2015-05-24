98988bb266c7b47a7b9654350c9e1a928e2e2f04
Faruk Ljuca <fljuca1@etf.unsa.ba>
2015-05-24 13:25:59 +0200

    radim sonar 3

--- glavna_aplikacija/src/main/java/ba/tim8/kvizbiz/dao/KlijentDao.java
+++ glavna_aplikacija/src/main/java/ba/tim8/kvizbiz/dao/KlijentDao.java
@@ -1,13 +1,11 @@
 package ba.tim8.kvizbiz.dao;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import ba.tim8.kvizbiz.entiteti.Administrator;
 import ba.tim8.kvizbiz.entiteti.Klijent;
 import ba.tim8.kvizbiz.konekcija.HibernateUtil;
 
