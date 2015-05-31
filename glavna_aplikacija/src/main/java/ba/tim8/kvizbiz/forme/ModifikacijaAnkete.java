778c0ca4117488ba46e76bcd031d75dd877ae7b7
akadrispahic <akadrispah1@etf.unsa.ba>
2015-05-31 13:16:37 +0200

    ispravljanje issue, mofifikacija ankete(buttoni)

--- glavna_aplikacija/src/main/java/ba/tim8/kvizbiz/forme/ModifikacijaAnkete.java
+++ glavna_aplikacija/src/main/java/ba/tim8/kvizbiz/forme/ModifikacijaAnkete.java
@@ -107,7 +107,7 @@ public class ModifikacijaAnkete extends JFrame {
 		kontejner.add(pnlPitanja);
 		
 		JButton btnDodaj = new JButton("Dodaj novo pitanje");
-		btnDodaj.setBounds(13, 23, 159, 23);
+		btnDodaj.setBounds(13, 23, 180, 23);
 		btnDodaj.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -158,7 +158,7 @@ public class ModifikacijaAnkete extends JFrame {
 		});
 			
 		JButton btnObrisi = new JButton("Obriši odabrano pitanje");
-		btnObrisi.setBounds(13, 129, 159, 23);
+		btnObrisi.setBounds(13, 129, 180, 23);
 		btnObrisi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Pitanje selected = new Pitanje();
@@ -233,7 +233,7 @@ public class ModifikacijaAnkete extends JFrame {
 		kontejner.add(btnOtkazi);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(176, 23, 381, 283);
+		scrollPane.setBounds(248, 20, 356, 283);
 		pnlPitanja.add(scrollPane);
 			
 		tblPitanja = new JTable();
@@ -281,7 +281,7 @@ public class ModifikacijaAnkete extends JFrame {
 				
 			}
 		});
-		btnNewButton.setBounds(13, 174, 159, 23);
+		btnNewButton.setBounds(13, 174, 180, 23);
 		pnlPitanja.add(btnNewButton);
 		
 		JButton btnNazad = new JButton("Nazad");
