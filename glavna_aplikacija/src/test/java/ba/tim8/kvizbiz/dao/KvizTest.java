ea97a501111e4d49f36c3eb57106e5621a80dfbb
edos11 <ekunic1@etf.unsa.ba>
2015-05-31 12:01:57 -0700

    KviizTest
    porpavljen

--- glavna_aplikacija/src/test/java/ba/tim8/kvizbiz/dao/KvizTest.java
+++ glavna_aplikacija/src/test/java/ba/tim8/kvizbiz/dao/KvizTest.java
@@ -13,14 +13,14 @@ import ba.tim8.kvizbiz.entiteti.Pitanje;
 
 public class KvizTest {
 	
-	/*
-	private static Kviz kviz1= new Kviz (1, "Kviz1", 25, true, true);
+	
+	private static Kviz kviz1= new Kviz ();
 	private static Set<Klijent> _klijenti = new HashSet<Klijent>();
 	private static Set<Pitanje> _pitanja = new HashSet<Pitanje>();
 	
 	@Test
 	public void test_get_id() {
-		
+		kviz1.set_id(1);
 		assertEquals(1, kviz1.get_id());
 	}
 	
@@ -32,12 +32,19 @@ public class KvizTest {
 	}
 	
 	@Test
-	public void test_naziv(){
+	public void test_naziv() throws Exception{
 		
 		kviz1.set_naziv("kviz2");
 		assertEquals("kviz2", kviz1.get_naziv());
 		
 	}
+	/*
+	@Test(expected=Exception.class)
+	public void set_naziv_izuzetak_test() throws Exception{
+		
+		kviz2.set_naziv("kviz2");
+		
+	}*/
 	
 	@Test
 	public void test_vremensko_ogranicenje() {
@@ -49,7 +56,7 @@ public class KvizTest {
 	
 	@Test
 	public void test_is_aktivan() {
-		
+		kviz1.set_aktivan(true);
 		assertEquals(true, kviz1.is_aktivan());
 		
 	}
@@ -76,5 +83,5 @@ public class KvizTest {
 		assertEquals(_pitanja, kviz1.get_pitanja());
 		
 	}
-	*/
+	
 }
